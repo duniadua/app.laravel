e6e129ef9de0d1b2442f993b3021aa2796de6965
duniadua <duniadua@gmail.com>
2013-04-24 22:28:18 +0700

    create new controller

---
+++ application/controllers/clubhouse.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of clubhouse
+ *
+ * @author sahid
+ */
+class Clubhouse_Controller extends Base_Controller {
+    //put your code here
+    public function action_index(){
+        
+    }
+    
+    public function action_add(){
+        
+    }
+    
+}
+
+?>
