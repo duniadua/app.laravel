15c3f583af28b05dc6e2734705403df1d289ecfe
duniadua <duniadua@gmail.com>
2013-04-24 22:18:02 +0700

    add fields

--- application/migrations/2013_04_20_000544_create_member.php
+++ application/migrations/2013_04_20_000544_create_member.php
@@ -2,24 +2,31 @@
 
 class Create_Member {
 
-	/**
-	 * Make changes to the database.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		//
-	}
+    /**
+     * Make changes to the database.
+     *
+     * @return void
+     */
+    public function up() {
+        //
+        Schema::create('member', function($table) {
 
-	/**
-	 * Revert the changes to the database.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+                    $table->increments('id');
+                    $table->string('nama', 32);
+                    $table->integer('usia');
+                    $table->string('password', 64);
+                    $table->timestamps();
+                });
+    }
+
+    /**
+     * Revert the changes to the database.
+     *
+     * @return void
+     */
+    public function down() {
+        //
+        Schema::drop('member');
+    }
 
 }
