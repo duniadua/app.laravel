39871eca73c908289889c675a1c6c04cf27c6302
duniadua <duniadua@gmail.com>
2013-04-24 22:33:59 +0700

    login controller

---
+++ application/controllers/account.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of account
+ *
+ * @author sahid
+ */
+class Account_Controller extends Base_Controller {
+    //put your code here
+    public function action_index(){
+        
+    }
+    
+    public function action_login(){
+        
+    }
+    
+    public function action_logout(){
+        
+    }
+}
+
+?>
