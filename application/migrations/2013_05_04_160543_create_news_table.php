4842c10de0a9cb95e7d786701397bff24c39fe43
duniadua <duniadua@gmail.com>
2013-05-04 23:19:42 +0700

    migration

---
+++ application/migrations/2013_05_04_160543_create_news_table.php
@@ -0,0 +1,38 @@
+<?php
+
+class Create_News_Table {
+
+    /**
+     * Make changes to the database.
+     *
+     * @return void
+     */
+    public function up() {
+//
+        Schema::create('news', function($table) {
+                    // auto incremental id (PK)
+                    $table->increments('id');
+                    // varchar 32
+                    $table->string('catagory', 20)->nullable();
+                    $table->string('title', 200)->nullable();
+                    $table->text('detail')->nullable();
+                    // int
+                    $table->integer('active')->default(0);
+                    // boolean
+                    $table->string('user',20)->nullable();
+                    $table->string('ip_address',60)->nullable();
+                    $table->timestamps();
+                });
+    }
+
+    /**
+     * Revert the changes to the database.
+     *
+     * @return void
+     */
+    public function down() {
+//
+        Schema::drop('users');
+    }
+
+}
