d88d85d921660ad44b33554589983c92a4fab087
duniadua <duniadua@gmail.com>
2013-05-09 14:07:10 +0700

    user model

---
+++ application/models/Users.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Users
+ *
+ * @author sahid
+ */
+class Users extends Eloquent {
+    //put your code here
+    public static $table = 'users';
+    public static $key = 'id';
+    
+}
+
+?>
