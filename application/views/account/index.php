d6c2b639881fa85c4599fdd6c8657694a98f790a
duniadua <duniadua@gmail.com>
2013-05-09 15:32:16 +0700

    create layout

--- application/views/account/index.php
+++ application/views/account/index.php
@@ -9,10 +9,33 @@
         ?>
     </head>
     <body>
-        <?php
-        // put your code here
-        echo Form::label('hello', 'Manage you account here');
-        ?>
+        <div class="container">
+            <div class="span5">
+                    <fieldset>
+                        <legend>News Update</legend>
+                        <label>User Name</label>
+                        <?php
+                        echo Form::open('account/add');
+                        echo Form::text('username');
+                        echo Form::label('pwd','Password');
+                        echo Form::password('password')."</br>";
+                        
+                        $active = array(
+                            'Y' => 'active',
+                            'N' => 'inactive',
+                        );
+                        
+                        echo Form::select('active',$active)."</br>";
+                        echo Form::submit('Save', array('class' => 'btn btn-primary'));
+                        echo Form::close();
+                        ?>
+                    </fieldset>
+            </div>
+            <div class="span5">
+
+            </div>
+        </div>
+
     </body>
     <?php
     echo render('asset.footer');
