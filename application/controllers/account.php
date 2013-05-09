ee8c93f41486f7db7f6d10e32de9f5a5b1cf0883
duniadua <duniadua@gmail.com>
2013-05-09 15:31:50 +0700

    fix add account

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -11,18 +11,36 @@
  * @author sahid
  */
 class Account_Controller extends Base_Controller {
+
     //put your code here
-    public function action_index(){
-        return  View::make('account.index');
+    public function action_index() {
+        return View::make('account.index');
     }
-    
-    public function action_login(){
+
+    public function action_login() {
         
     }
-    
-    public function action_logout(){
+
+    public function action_logout() {
         
     }
+
+    public function action_add() {
+
+        $data = array(
+            'username' => Input::get('username'),
+            'passsword' => Input::get('password'),
+            'email' => 'duniadua@gmail.com',
+            'role' => 'user',
+            'active' => Input::get('active'),
+        );
+
+        $news = new News($data);
+        $news->save();
+
+        return Redirect::to('account');
+    }
+
 }
 
 ?>
