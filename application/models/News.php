91be6e1a7ea3240c7531bf27d407eee252eb5609
duniadua <duniadua@gmail.com>
2013-05-09 14:08:42 +0700

    news model

---
+++ application/models/News.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of News
+ *
+ * @author sahid
+ */
+class News extends Eloquent {
+
+    //put your code here
+    public static $table = 'news';
+    public static $key = 'id';
+
+}
+
+?>
