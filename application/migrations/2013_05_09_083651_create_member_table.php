371b90e105f0c0622550ffbfc97b2d19e58c8197
duniadua <duniadua@gmail.com>
2013-05-09 18:41:55 +0700

    regenerate old table

---
+++ application/migrations/2013_05_09_083651_create_member_table.php
@@ -0,0 +1,32 @@
+<?php
+
+class Create_Member_Table {
+
+    /**
+     * Make changes to the database.
+     *
+     * @return void
+     */
+    public function up() {
+        //
+        Schema::create('member', function($table) {
+
+                    $table->increments('id');
+                    $table->string('nama', 32);
+                    $table->integer('usia');
+                    $table->string('password', 64);
+                    $table->timestamps();
+                });
+    }
+
+    /**
+     * Revert the changes to the database.
+     *
+     * @return void
+     */
+    public function down() {
+        //
+        Schema::drop('member');
+    }
+
+}
